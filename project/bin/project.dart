82dfc0a67052451adf6d14da35ee0058ce918323
Ruba10 <albalawi.Ruba1@gmail.com>
2022-11-19 02:20:20 +0300

    sell,edit,viewlibrary

--- project/bin/project.dart
+++ project/bin/project.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 List<Book> Books = [
   Book(
@@ -13,7 +14,7 @@ List<Book> Books = [
       quantity: 22),
   Book(
       book_title: "Clean Code",
-      author: "Robert Cecil Martin",
+      author: "Rpbert Cecil Martin",
       price: 50,
       quantity: 5),
   Book(
@@ -28,48 +29,39 @@ List<Book> Books = [
       quantity: 9)
 ];
 int counter = 1;
-
+List cart = [];
 void main() {
   print("Welcom to pur library");
+  //add();
+  //search();
+  // print(Books.length);
+  // delete();
+  // print(Books.length);
 }
 
 abstract class Library {
-  add();
-  delete();
   edit();
   viewInformation();
-  search();
   sell();
 }
 
 class Book extends Library {
-  int? book_id = 0;
+  int book_id = 0;
   String? book_title;
   String? author;
   double? price;
-  int? quantity;
-  Book({this.book_title, this.author, this.price, this.quantity}) {
+  late int quantity;
+  Book(
+      {required this.book_title,
+      required this.author,
+      required this.price,
+      required this.quantity}) {
     this.book_id = counter;
     counter++;
   }
 
-  get book => null;
-
   @override
-  add() {
-    print("Enter book name: ");
-    String? newtitle = book_title = stdin.readLineSync();
-    print("Enter auther name: ");
-    String? newauthor = author = stdin.readLineSync();
-    print("Enter book price : ");
-    //int? newprice = price = int.parse(stdin.readLineSync());
-    print("Enter quantity of book : ");
-    //double? newq = quantity =double.parse(stdin.readLineSync());
-  }
-
   @override
-  delete() {}
-
   @override
   edit() {
     var change;
@@ -82,33 +74,160 @@ class Book extends Library {
     } else if (change == 2) {
       author = stdin.readLineSync();
     } else if (change == 3) {
-      // price = int.parse(stdin.readLineSync());
+      double price = double.parse(stdin.readLineSync()!);
     } else if (change == 4) {
-      // quantity = stdin.readLineSync();
+      int quantity = int.parse(stdin.readLineSync()!);
     }
   }
 
   @override
-  search() {}
+  sell() {
+    var check;
+    print("which book you want to buy");
+    print(viewInformation());
+
+    print("now enter the quantity");
+    int count = int.parse(stdin.readLineSync()!);
+
+    check = price! * count;
+
+    if (quantity > 0) {
+      quantity--;
+      print("Thank you for your Shopping");
+    } else {
+      print("Sorry! we are out of stock");
+    }
+  }
+
+  @override
+  viewInformation() {
+    print(
+        "The ID is $book_id\tThe Title is $book_title\tThe author $author\tThe price is $price\tThe quantity is $quantity ");
+  }
 }
 
-@override
-sell() {}
+add() {
+  String? title;
+  String? author;
+  double? price;
+  int quantity = 0;
+  bool adding = true;
 
-@override
-viewInformation() {
-  print(
-      "1- Book name:Start With Why  Auther:simon sink  price:80.0  quantity: 13 ");
+  while (adding) {
+    print("Enter Book Title");
+    title = stdin.readLineSync();
+    if (title != "") {
+      adding = false;
+    }
+  }
+  adding = true;
+  while (adding) {
+    print("Enter Book Author");
+    author = stdin.readLineSync()!;
+    if (author != "") {
+      adding = false;
+    }
+  }
+  adding = true;
+  while (adding) {
+    print("Enter Book Price");
+    price = double.parse(stdin.readLineSync()!);
+    if (price > 0) {
+      adding = false;
+    }
+  }
+  adding = true;
+  while (adding) {
+    print("Enter Book Quantity");
+    quantity = int.parse(stdin.readLineSync()!);
+    if (quantity > 0) {
+      adding = false;
+    }
+  }
+  Books.add(Book(
+      book_title: title, author: author, price: price, quantity: quantity));
+}
 
-  print(
-      "2- Book name:But how do it know  Auther:J.Clark Scott  price:59.9  quantity: 22 ");
+look(int search) {
+  var booksearch;
+  bool se = true;
+  if (search == 1) {
+    while (se) {
+      print("Enter Book ID");
+      booksearch = int.parse(stdin.readLineSync()!);
+      for (int i = 0; i < Books.length; i++) {
+        var item = Books[i].book_id;
+        if (item == booksearch) {
+          Books[i].viewInformation();
+          se = false;
+        }
+      }
+    }
+  } else if (search == 2) {
+    while (se) {
+      print("Enter Book Title");
+      booksearch = stdin.readLineSync()!;
+      for (int i = 0; i < Books.length; i++) {
+        var item = Books[i].book_title;
+        if (item!.contains(booksearch)) {
+          // if (item == booksearch) for exact Match
+          Books[i].viewInformation();
+          se = false;
+        }
+      }
+    }
+  } else if (search == 3) {
+    while (se) {
+      print("Enter Book Autor");
+      booksearch = stdin.readLineSync()!;
+      for (int i = 0; i < Books.length; i++) {
+        var item = Books[i].author;
+        if (item!.contains(booksearch)) {
+          // // if (item == booksearch) for exact Match
+          Books[i].viewInformation();
+          se = false;
+        }
+      }
+    }
+  }
+}
 
-  print(
-      "3- Book name:Clean Code Auther:Rpbert Cecil Martin  price:50.0  quantity: 5 ");
+search() {
+  bool searching = true;
+  int search;
+  while (searching) {
+    print("How do you want to Search\n1 By ID\n2 By Title \n3 By Author");
+    search = int.parse(stdin.readLineSync()!);
+    if (search == 1 || search == 2 || search == 3 || search == 4) {
+      look(search);
+      searching = false;
+    }
+  }
+}
 
-  print(
-      "4- Book name:Zero to One  Auther:Peter Thiel price:45.0  quantity: 12 ");
+delete() {
+  bool deleting = true;
+  int deleteID;
+  String deleteMessage = "Enter Book ID for deleting it ";
+  while (deleting) {
+    print(deleteMessage);
+    deleteID = int.parse(stdin.readLineSync()!);
+    for (int i = 0; i < Books.length; i++) {
+      int x = Books[i].book_id;
+      if (x == deleteID) {
+        Books.remove(Books[i]);
+        deleting = false;
+      }
+      deleteMessage = "Re enter a Valid ID";
+    }
+  }
+}
 
-  print(
-      "5- Book name:You dont know JS  Auther:Kyle Simpson  price:39.9  quantity: 9 ");
+viewLibrary() {
+  for (int i = 0; i == Books.length; i++) {
+    print(
+        "The ID is ${Books[i].book_id} tThe Title is  ${Books[i].book_title} The author name is  ${Books[i].author} The price is ${Books[i].price} The quantity is ${Books[i].quantity}");
+    var viewprice = Books[i]
+        .price; //متغير يحدد لي قيمة الكتاب , عشان نستخدمة في حساب الفاتورة
+  }
 }
